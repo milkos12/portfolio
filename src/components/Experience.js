3bc3d681a95bbc868bfc1c62e677087c99d76095
Miller Ostos <millerandresoc@gmail.com>
2024-12-18 15:51:43 -0500

    feat Experiencie: add details and gif about the projects maked in vele+

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,31 +5,60 @@ export const Experience = () => {
         <div className="experience">
             <h1>Experience</h1>
             <div className='experience-content'>
-            <div className='experience-content-description'>
+                <div className='experience-content-description'>
                     <div>
                         <h5>2024<wbr /> — <wbr />Present</h5>
                     </div>
                     <div>
                         <h5>Software Developer &nbsp;&nbsp;·&nbsp;&nbsp; VelezReyez+</h5>
-                        <p>
-                        I participated in developing an alluvial visualization from scratch using the D3.js library. The goal was to graphically represent inequality groups across different time ranges. My contribution included the design and implementation of the alluvial metric and in the refactoring for the correct resizing of the tree graph</p>
-                        <div className='experience-content-experience-tecnologies'>
+                        <p>VélezReyes+ is the philanthropic platform, focused on education and democracy to expand opportunities in Latin America.</p>
+                        <div className='experience-content-details'>
+                            <h4 style={{ color: '#FFFFFF' }} >Project&nbsp;&nbsp;·&nbsp;&nbsp;GEOM</h4>
+                            <p>Data visualization tool to understand inequality of opportunities worldwide.</p>
                             <div>
-                                <p>JavaScript</p>
+                                <div className='projects-content-conteiner-title experience-content-details-title'>
+                                    <h5 style={{ color: '#FFFFFF' }} >Alluvial graph</h5>
+                                    <a href='https://geom.ecineq.org/country-profile/' target='_blank' style={{ color: '#ffffff' }} className='experience-content-details-title-svg'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className='projects-content-links'><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline className='projects-content-links-arrow' points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                    </a>
+                                </div>
+                                <p>Collaborated with a team to transform inequality data into a compelling alluvial visualization using D3.js. Designed metrics and implemented adaptive resizing, making the interactive alluvial graph both insightful and responsive.</p>
+                                <img className='projects-content-img experience-content-details-img' src='/gifs/alluvial-graph.gif'></img>
+                                <div className='experience-content-experience-tecnologies'>
+                                    <div>
+                                        <p>JavaScript</p>
+                                    </div>
+                                    <div>
+                                        <p>React</p>
+                                    </div>
+                                    <div>
+                                        <p>D3.js</p>
+                                    </div>
+                                </div>
                             </div>
                             <div>
-                                <p>React</p>
-                            </div>
-                            <div>
-                                <p>D3.js</p>
-                            </div>
-                            <div>
-                                <p>Python</p>
-                            </div>
-                            <div>
-                                <p>CRMs</p>
+                                <div className='projects-content-conteiner-title experience-content-details-title'>
+                                    <h5 style={{ color: '#FFFFFF' }} >Tree graph (Refactor)</h5>
+                                    <a href='https://geom.ecineq.org/country-profile/' target='_blank' style={{ color: '#ffffff' }} className='experience-content-details-title-svg'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className='projects-content-links'><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline className='projects-content-links-arrow' points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                    </a>
+                                </div>
+                                <p>Optimized the graph's positioning to fit correctly within the available space, enhancing its visualization.</p>
+                                <img className='projects-content-img experience-content-details-img' src='/gifs/refactor-tree-graph-v2.gif'></img>
+                                <div className='experience-content-experience-tecnologies '>
+                                    <div>
+                                        <p>JavaScript</p>
+                                    </div>
+                                    <div>
+                                        <p>React</p>
+                                    </div>
+                                    <div>
+                                        <p>D3.js</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
                 <div className='experience-content-description'>
