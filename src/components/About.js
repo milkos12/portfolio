96749dd320367dd7f5ee7dc387303ce7a7090257
Miller Ostos <millerandresoc@gmail.com>
2025-01-09 09:36:02 -0500

    add Experience: fead add new details and stiky position title

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,9 @@ export const About = () => {
         <div className='about' >
             <h4>ABOUT</h4>
             <p>
-            Software engineer experienced in using APIs, Apps Script, and process automation. Proficient in HTML and CSS with React to create dynamic and responsive interfaces. Passionate about optimizing and improving processes through technological solutions, integrating and automating any task.
+            With over a year of experience in React JS, and technologies such as JavaScript, APIs, and No-code tools, I have worked on process automation and data visualization, optimizing operational efficiency in projects.
+            <br />
+            <br />
             </p>
         </div>
     );
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,17 +1,27 @@
 import './Experience.css'
+import InstagramPost from './InstagramPost';
 
 export const Experience = () => {
     return (
         <div className="experience">
             <h1>Experience</h1>
+            <h3><a href='https://velezreyesmas.com/' target='_blank' style={{ color: '#ffffff', textDecoration: 'none' }} className='experience-content-details-title-svg'>
+                vélezreyez+&nbsp;&nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className='projects-content-links' style={{ maxWidth: '18px' }}><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline className='projects-content-links-arrow' points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+            </a></h3>
+            <p style={{ fontWeight: '200' }}>VélezReyes+ is the philanthropic platform, focused on education and democracy to expand opportunities in Latin America.</p>
             <div className='experience-content'>
+
                 <div className='experience-content-description'>
-                    <div>
-                        <h5>2024<wbr /> — <wbr />Present</h5>
+                    <div >
+                        <div className='experience-content-sticky-title' style={{backgroundColor:'#000000', height: '50px', paddingLeft: '5px', margin: '0', borderRadius: '0 0 10px 10px', paddingTop: '.5px', zIndex:'12', marginLeft: '-10px', marginRight: '-8px'}}>
+                            <h5>2024<wbr /> — <wbr />Present</h5>
+                        </div>
+                        
                     </div>
                     <div>
-                        <h5>Software Developer &nbsp;&nbsp;·&nbsp;&nbsp; VelezReyez+</h5>
-                        <p>VélezReyes+ is the philanthropic platform, focused on education and democracy to expand opportunities in Latin America.</p>
+                        <div className='experience-content-sticky-title' style={{backgroundColor:'#000000', height: '50px', paddingLeft: '5px', margin: '0', borderRadius: '0 0 10px 10px', paddingTop: '.5px', zIndex:'12', marginLeft: '-10px', marginRight: '-8px', boxShadow: '-1px 15px 20px 1px rgba(0,0,0,0.75)'}}><h5>Software Developer &nbsp;&nbsp;·&nbsp;&nbsp; VelezReyez+</h5></div>
+                        
                         <div className='experience-content-details'>
                             <h4 style={{ color: '#FFFFFF' }} >Project&nbsp;&nbsp;·&nbsp;&nbsp;GEOM</h4>
                             <p>Data visualization tool to understand inequality of opportunities worldwide.</p>
@@ -78,6 +88,37 @@ export const Experience = () => {
                                     </div>
                                 </div>
                             </div>
+                            <div>
+                                <h4 style={{ color: '#FFFFFF', marginTop: '50px' }} >Project&nbsp;&nbsp;·&nbsp;&nbsp;Ver+ Tech Scholarship</h4>
+                                <p>supports low-income high school graduates by providing access to a systems engineering undergraduate program at high-quality universities.</p>
+                                <div className='projects-content-conteiner-title experience-content-details-title'>
+                                    <h5 style={{ color: '#FFFFFF' }} >Second Cohort of the Ver+ Tech Scholarship</h5>
+                                    <a href='https://velezreyesmas.com/primera-cohorte-beca-tech/' target='_blank' style={{ color: '#ffffff' }} className='experience-content-details-title-svg'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className='projects-content-links'><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline className='projects-content-links-arrow' points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                    </a>
+                                </div>
+                                <p>Provided tech support for the entire registration process, handling over <b>5,000 participants from 31 of Colombia’s 32 departments.</b> Automated filtering and selection in the initial phases using CRM and other tools, enabling real-time tracking of participants and key metrics throughout the process.</p>
+                                <div>
+                                    <InstagramPost></InstagramPost>
+                                </div>
+                                <div className='experience-content-experience-tecnologies '>
+                                    <div>
+                                        <p>CRM (zoho)</p>
+                                    </div>
+                                    <div>
+                                        <p>API</p>
+                                    </div>
+                                    <div>
+                                        <p>JavaScript</p>
+                                    </div>
+                                    <div>
+                                        <p>No-Code</p>
+                                    </div>
+                                    <div>
+                                        <p>Looker Studio</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
@@ -88,6 +129,7 @@ export const Experience = () => {
                     </div>
                     <div>
                         <h5>Tech Fellow &nbsp;&nbsp;·&nbsp;&nbsp; VelezReyez+</h5>
+                        <p><a href='https://velezreyesmas.com/tech-fellowship/' target='_blank' style={{ color: '#ffffff' }} >Fellow Tech Fellowship</a> is an acceleration program for tech talent. <b>Out of 1,500 applicants, 39 finalists</b> were selected after passing rigorous technical tests and interviews, receiving training from industry experts and were hired by leading companies</p>
                         <p>
                             I develop software in a team using technologies such as AWS, JavaScript, and Python to implement cloud solutions. I manage cloud deployments with GitHub Actions CI/CD. Under the guidance of experts, I am developing in the world of technology in Latin America.</p>
                         <div className='experience-content-experience-tecnologies'>
@@ -110,6 +152,11 @@ export const Experience = () => {
                     </div>
                 </div>
                 <div>
+                    <h3><a href='https://www.ubits.com/' target='_blank' style={{ color: '#ffffff', textDecoration: 'none' }} className='experience-content-details-title-svg'>
+                        UBITS&nbsp;&nbsp;
+                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className='projects-content-links' style={{ maxWidth: '18px' }}><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline className='projects-content-links-arrow' points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                    </a></h3>
+                    <p>Ubits is an online learning platform that offers business training and professional development courses in key areas such as leadership, technology, and digital skills.</p>
                     <div className='experience-content-description'>
                         <div>
                             <h5>oct 2022<wbr /> — <wbr />nov 2022</h5>
